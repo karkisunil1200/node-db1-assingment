4687366a17ca9f674fc76000ad2caf8571758bd6
Sunil Karki <karkisunil1200@gmail.com>
2020-04-13 15:42:16 -0500

    Initial commit

--- queries.sql
+++ queries.sql
@@ -1,10 +1,18 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT *
+FROM customers
+WHERE PostalCode = 1010
+
 
 -- Find the phone number for the supplier with the id 11
+SELECT Phone
+FROM SUPPLIERS
+WHERE SupplierID = 11
 
 -- List first 10 orders placed, sorted descending by the order date
+
 
 -- Find all customers that live in London, Madrid, or Brazil
 
